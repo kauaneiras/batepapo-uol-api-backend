f02b17cd361c69f9087ad02e8f4a4932adacd5fa
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-11-12 09:36:09 -0300

    POST/participants 2/6
    Receives a name from the Body and validates if the name is not empty

--- src/index.js
+++ src/index.js
@@ -12,6 +12,31 @@ app.use(cors());
 
 dotenv.config();
 const mongoClient = new MongoClient(process.env.MONGO_URL);
+let mongoData = mongoClient.db("bate-papo");
+
+//data validation
+//{name: 'João', lastStatus: 12313123} // O conteúdo do lastStatus será explicado nos próximos requisitos
+const schemaName = joi.object({ name: joi.string().min(2).max(30).required()});
+//{from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
+const schemaMessage = joi.object({ 
+    from: joi.string().min(2).max(30).required(),
+    to: joi.string().min(2).max(30).required(),
+    text: joi.string().min(1).max(1000).required(),
+    type: joi.string().required(),
+    time: joi,
+});
+
+//POST /participants
+app.post("/participants", (req, req )=>{
+    const {name} = req.body;
+    const validation = schemaName.validate({name});
+
+    if(validation.error){
+        res.status(422).send("nome inválido");
+        return;
+    }
+
+});
 
 
 app.listen(5000, () => {console.log("Server is running on port 5000")});
