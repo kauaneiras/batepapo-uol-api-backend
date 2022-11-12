1588e30535b12530288d56ac867fa43f908aada8
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-11-12 08:39:09 -0300

    Add Mongo
    mongo has been added on App

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import joi from 'joi';
 
-dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
+dotenv.config();
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+
 
 app.listen(5000, () => {console.log("Server is running on port 5000")});
 
